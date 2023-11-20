32a7c96706b88be30a4c4bd7f49d8d3dae8c32c0
YourName <jonas.sundberg@slu.se>
2023-11-20 13:48:25 +0100

    added work mat

---
+++ code/yolo_inference.py
@@ -0,0 +1,79 @@
+
+
+from ultralytics import YOLO
+import os
+import sys 
+import pathlib
+import numpy as np
+from PIL import Image
+import pandas as pd
+
+# Load a pretrained YOLO model
+model = YOLO('yolov8n.pt')
+
+#files = os.listdir("images/")
+#files = [pathlib.Path("images/"+item) for item in files]
+
+
+# Perform object detection 
+vids = ["Auklab1_ROST6_2023-07-06_06.41.01.mp4", "Auklab1_ROST6_2023-07-06_08.53.17.mp4"]
+fps = 25
+
+for vid in vids: 
+
+    # Pick out relevant video information
+    name = vid.split("_")
+    time = name[3].split(".")
+    ledge = name[1]
+
+    starttime = pd.to_datetime(name[2]+" "+time[0]+":"+time[1]+":"+time[2])
+    starttime_u = starttime.timestamp()
+
+    results = model(f'vids2/{vid}', stream = True)
+
+    # Process results list
+    time = []
+    boxes = []
+    confs = []
+    classes = []
+    framenum = []
+    counter = starttime_u
+    counterx = 0
+
+    for r in results:
+
+        boxes.append(r.boxes.xyxy.tolist()) 
+        classes.append(r.boxes.cls.tolist())
+        confs.append(r.boxes.conf.tolist())
+        ndetect = len(r.boxes.conf.tolist())
+        time.append([counter] * ndetect)
+        framenum.append([counterx] * ndetect)
+
+        # Plot
+        #im_array = r.plot()  # plot a BGR numpy array of predictions
+        #im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+        #im.show()  # show image
+        
+        counter += 1/fps
+        counterx += 1
+
+    # Concatenate outputs
+    conf = sum(confs, [])
+    classes = sum(classes, [])
+    boxesx = sum(boxes, [])
+    times = sum(time, [])
+    framenums = sum(framenum, [])
+
+
+    # Save as data frames
+    out1 = pd.DataFrame(boxesx, columns = ["x", "y", "w", "h"])
+    out2 = pd.DataFrame(list(zip(classes, conf, times, framenums)), columns = ["class", "conf", "time", "frame"])
+
+    out = out1.merge(out2, left_index = True, right_index = True)
+    out["ledge"] = ledge
+    out["filename"] = vid
+
+    out.to_csv(f'{vid}.csv')
+
+
+
---
+++ code/yolo_minimal.py
@@ -0,0 +1,17 @@
+
+
+
+from ultralytics import YOLO
+
+# Load a pretrained YOLO model
+model = YOLO('yolov8n.pt')
+
+# Perform object detection on an image using the model
+results = model.track(source="https://youtu.be/LNwODJXcvt4")
+
+## Process results list
+#for r in results:
+#    boxes = r.boxes.xyxy  # Boxes object for bbox outputs
+    
+
+
