7877d458ddc06363787297c77e17ab6564e7425a
hentatisundberg <jonas.sundberg@slu.se>
2023-11-27 16:54:08 +0100

    update train

--- code/yolov8/yolo_train.py
+++ code/yolov8/yolo_train.py
@@ -1,23 +1,12 @@
 
 from ultralytics import YOLO
-import os
-import sys 
-import pathlib
-import numpy as np
-from PIL import Image
-import pandas as pd
-import torch
-
-
-
-from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8n.yaml')  # build a new model from YAML
-#model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+#model = YOLO('yolov8n.yaml')  # build a new model from YAML
+model = YOLO('../models/yolov8m.pt')  # load a pretrained model (recommended for training)
 #model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data='../data/data.yaml', epochs=5, imgsz=640)
+results = model.train(data='../data/data.yaml', epochs=50, imgsz=1280)
 
 
