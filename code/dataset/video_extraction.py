d22d3d42fcfa74bd2cde2527f9b231704fc10f1b
hentatisundberg <jonas.sundberg@slu.se>
2023-11-27 16:52:14 +0100

    update vid cutting

--- code/dataset/video_extraction.py
+++ code/dataset/video_extraction.py
@@ -1,37 +1,44 @@
 import os
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import pandas as pd
+import sys
 
 
-video_metadata = [
-    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:02:14', '04:03:29'],
-    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:07:12', '04:10:55'],
-    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:11:32', '04:12:02'],
-    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:32:54', '04:33:10'],
-    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:54:00', '04:54:15'],
-    ['Auklab1_BONDEN6_2022-07-02_05.00.00.mp4', '05:44:23', '05:44:40'],
-    ['Auklab1_ROST6_2022-07-02_04.00.00.mp4', '04:09:44', '04:10:26'],
-    ['Auklab1_ROST6_2022-07-02_05.00.00.mp4', '05:03:57', '05:04:06'],
-    ['Auklab1_ROST6_2022-07-02_05.00.00.mp4', '05:26:25', '05:28:50'],
-    ['Auklab1_FAR6_2022-07-02_04.00.00.mp4', '04:46:44', '04:58:58'],
-    ['Auklab1_FAR6_2022-07-02_03.00.00.mp4', '03:44:24', '03:46:17'],
-    ['Auklab1_ROST3_2022-07-02_04.00.00.mp4', '04:10:25', '04:10:37'],
-    ['Auklab1_ROST3_2022-07-02_04.00.00.mp4', '04:57:33', '04:58:47'],
-    ['Auklab1_ROST3_2022-07-02_03.00.00.mp4', '03:35:48', '03:36:03'],
-    ['Auklab1_TRI3_2022-07-02_03.00.00.mp4', '03:36:30', '03:36:40'],
-    ['Auklab1_TRI6_2022-07-02_03.00.00.mp4', '03:00:00', '03:06:15'],
-]
 
-nas_video_path = '/home/shreyash_kad/mnt/nas2/BSP_data/Video/'
-output_folder = '/home/shreyash_kad/fish_detection/dataset/fish_video'
+video_meta = pd.read_csv("../data/fishvids.csv")
+
+
+#video_metadata = [
+#    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:02:14', '04:03:29'],
+#    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:07:12', '04:10:55'],
+##    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:11:32', '04:12:02'],
+#   ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:32:54', '04:33:10'],
+#   ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:54:00', '04:54:15'],
+#   ['Auklab1_BONDEN6_2022-07-02_05.00.00.mp4', '05:44:23', '05:44:40'],
+#   ['Auklab1_ROST6_2022-07-02_04.00.00.mp4', '04:09:44', '04:10:26'],
+#   ['Auklab1_ROST6_2022-07-02_05.00.00.mp4', '05:03:57', '05:04:06'],
+#   ['Auklab1_ROST6_2022-07-02_05.00.00.mp4', '05:26:25', '05:28:50'],
+#   ['Auklab1_FAR6_2022-07-02_04.00.00.mp4', '04:46:44', '04:58:58'],
+#   ['Auklab1_FAR6_2022-07-02_03.00.00.mp4', '03:44:24', '03:46:17'],
+#   ['Auklab1_ROST3_2022-07-02_04.00.00.mp4', '04:10:25', '04:10:37'],
+#   ['Auklab1_ROST3_2022-07-02_04.00.00.mp4', '04:57:33', '04:58:47'],
+#   ['Auklab1_ROST3_2022-07-02_03.00.00.mp4', '03:35:48', '03:36:03'],
+#   ['Auklab1_TRI3_2022-07-02_03.00.00.mp4', '03:36:30', '03:36:40'],
+#   ['Auklab1_TRI6_2022-07-02_03.00.00.mp4', '03:00:00', '03:06:15'],
+#]
+
+nas_video_path = "../../../../../../mnt/BSP_NAS1/Video/Video2023/TRI3/2023-07-03/"
+
+output_folder = '../vids'
 
 if not os.path.isdir(output_folder):
     os.mkdir(output_folder)
 
 
-for metadata in video_metadata:
-    _, ledge_name, video_date, start_hour = metadata[0].split('_')
-    year = video_date.split('-')[0]
-    start_hour = int(start_hour[:2])
+for metadata in video_meta.iloc[0]:
+#    _, ledge_name, video_date, start_hour = metadata[0].split('_')
+#    year = video_date.split('-')[0]
+#    start_hour = int(start_hour[:2])
     clip_start_hour, clip_start_min, clip_start_sec = metadata[1].split(':')
     clip_start_hour, clip_start_min, clip_start_sec = int(clip_start_hour), int(clip_start_min), int(clip_start_sec)
     clip_end_hour, clip_end_min, clip_end_sec = metadata[2].split(':')
