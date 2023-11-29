2a07a7f3a9e67c1de8abb532f04fa6450c3af3af
YourName <jonas.sundberg@slu.se>
2023-11-29 19:48:04 +0100

    Semi-complete auto annotate pipeline

--- code/dataset/video_extraction.py
+++ code/dataset/video_extraction.py
@@ -3,64 +3,88 @@ from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import pandas as pd
 import sys
 import numpy as np
+import cv2
+from autodistill_yolov8 import YOLOv8Base
+from autodistill.detection import CaptionOntology
 
 
-video_meta = pd.read_csv("../data/fishvids.csv")
+# Read arguments
+video_meta_path = sys.argv[1]
+datfold = sys.argv[2]
+vid_outfold = sys.argv[3]
+im_outfold = sys.argv[4]
 
 
-#video_metadata = [
-#    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:02:14', '04:03:29'],
-#    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:07:12', '04:10:55'],
-##    ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:11:32', '04:12:02'],
-#   ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:32:54', '04:33:10'],
-#   ['Auklab1_BONDEN6_2022-07-02_04.00.00.mp4', '04:54:00', '04:54:15'],
-#   ['Auklab1_BONDEN6_2022-07-02_05.00.00.mp4', '05:44:23', '05:44:40'],
-#   ['Auklab1_ROST6_2022-07-02_04.00.00.mp4', '04:09:44', '04:10:26'],
-#   ['Auklab1_ROST6_2022-07-02_05.00.00.mp4', '05:03:57', '05:04:06'],
-#   ['Auklab1_ROST6_2022-07-02_05.00.00.mp4', '05:26:25', '05:28:50'],
-#   ['Auklab1_FAR6_2022-07-02_04.00.00.mp4', '04:46:44', '04:58:58'],
-#   ['Auklab1_FAR6_2022-07-02_03.00.00.mp4', '03:44:24', '03:46:17'],
-#   ['Auklab1_ROST3_2022-07-02_04.00.00.mp4', '04:10:25', '04:10:37'],
-#   ['Auklab1_ROST3_2022-07-02_04.00.00.mp4', '04:57:33', '04:58:47'],
-#   ['Auklab1_ROST3_2022-07-02_03.00.00.mp4', '03:35:48', '03:36:03'],
-#   ['Auklab1_TRI3_2022-07-02_03.00.00.mp4', '03:36:30', '03:36:40'],
-#   ['Auklab1_TRI6_2022-07-02_03.00.00.mp4', '03:00:00', '03:06:15'],
-#]
+video_meta = pd.read_csv(video_meta_path)
 
-nas_video_path = "../../../../../../mnt/BSP_NAS1/Video/Video2023/TRI3/2023-07-03/"
-ext_video_path = "../../../../../../../../Volumes/JHS-SSD2/2023-07-03"
+def cut_vid(): 
 
-#print(os.listdir(testdir))
-#sys.exit()
+    for ind in video_meta.index:
 
-output_folder = '../vids/'
+        viddat = video_meta.iloc[ind]
 
-#if not os.path.isdir(output_folder):
-#    os.mkdir(output_folder)
+        if viddat["Done"] != "x":
+
+            non, ledge_name, video_date, start_hour = viddat[0].split('_')
+            year = video_date.split('-')[0]
+            start_hour = int(start_hour[:2])
+
+            startclip = pd.to_datetime(video_date+" "+viddat[1])
+            endclip = pd.to_datetime(video_date+" "+viddat[2])
+            startvid = startclip.floor("H")
+
+            startsec = (startclip-startvid)/np.timedelta64(1,'s')
+            endsec = (endclip-startvid)/np.timedelta64(1,'s')
+
+            filename_out = vid_outfold+viddat[0][:-4]+"_"+str(int(startsec))+"_"+str(int(endsec))+".mp4"
+            #filename_out = viddat[0][:-4]+"_"+str(int(startsec))+"_"+str(int(endsec))+".mp4"
+
+            ffmpeg_extract_subclip(
+            #    os.path.join(nas_video_path, 'Video'+year, ledge_name, video_date, metadata[0]),
+                os.path.join(datfold, viddat[0]),
+                startsec,
+                endsec,
+                targetname = filename_out
+            )
+
+def save_all_frames(ext='jpg'):
+    cap = cv2.VideoCapture(os.path.join(vid_outfold+file))
+
+    if not cap.isOpened():
+        return
+
+    digit = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
+
+    n = 0
+
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            cv2.imwrite('{}_{}.{}'.format(os.path.join(im_outfold+file), str(n).zfill(digit), ext), frame)
+            n += 1
+        else:
+            return
 
 
-for ind in video_meta.index:
 
-    viddat = video_meta.iloc[ind]
+## RUN
 
-    non, ledge_name, video_date, start_hour = viddat[0].split('_')
-    year = video_date.split('-')[0]
-    start_hour = int(start_hour[:2])
+# Run video cutting
+results = cut_vid() 
 
-    startclip = pd.to_datetime(video_date+" "+viddat[1])
-    endclip = pd.to_datetime(video_date+" "+viddat[2])
-    startvid = startclip.floor("H")
+# Extract frames from all vids 
+for file in os.listdir(vid_outfold)[1:]:
+    save_all_frames()
 
-    startsec = (startclip-startvid)/np.timedelta64(1,'s')
-    endsec = (endclip-startvid)/np.timedelta64(1,'s')
+# Annotate
+base_model = YOLOv8Base(ontology=CaptionOntology({"fish": "fish"}), weights_path="../models/best.pt")
 
-    filename_out = output_folder+viddat[0][:-4]+"_"+str(int(startsec))+"_"+str(int(endsec))+".mp4"
-    #filename_out = viddat[0][:-4]+"_"+str(int(startsec))+"_"+str(int(endsec))+".mp4"
+base_model.label(
+  input_folder=im_outfold,
+  output_folder="../data"
+)
 
-    ffmpeg_extract_subclip(
-    #    os.path.join(nas_video_path, 'Video'+year, ledge_name, video_date, metadata[0]),
-        os.path.join(ext_video_path, viddat[0]),
-        startsec,
-        endsec,
-        targetname = filename_out
-    )
+
+
+#python3 -i dataset/video_extraction.py "../data/fishvids.csv" "../../../../../../../../Volumes/JHS-SSD2/2023-07-03" "../vids/" "../images/"
+
