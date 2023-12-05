6938e44a44b2a07377a2efcfb0ad35dd7824a5c7
hentatisundberg <jonas.sundberg@slu.se>
2023-12-05 14:46:19 +0100

    update tune settings

--- code/yolov8/yolo_tune.py
+++ code/yolov8/yolo_tune.py
@@ -2,14 +2,16 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('../runs/train15/weights/best.pt')  # load a pretrained model (recommended for training)
+model = YOLO('runs/detect/train20/weights/best.pt')  # load the best model so far
 
 # Tune hyper-parameters 
 model.tune(data='../config/data.yaml', 
-    epochs=30, 
-    iterations = 300, 
+    epochs=20, 
+    iterations = 30, 
     optimizer = 'AdamW', 
     plots = False, 
-    save = False, 
-    val = False)
+    save = True, 
+    val = False, 
+    device = [0, 1])
+
 
