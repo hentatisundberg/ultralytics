7d2ffa90424383937a4419b42ae3f9cdb74ce675
YourName <jonas.sundberg@slu.se>
2023-12-08 16:22:08 +0100

    update fishvids

--- code/postprocess/show_detections.py
+++ code/postprocess/show_detections.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-dat = pd.read_csv("inference/Auklab1_FAR3_2022-07-08_04.00.00.mp4.csv")
+dat = pd.read_csv("inference/Auklab1_FAR3_2022-07-08_05.00.00.mp4.csv")
 dat["time2"] = pd.to_datetime(dat["time"]*1000*1000*1000)
 
 fig, ax = plt.subplots()
