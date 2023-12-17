0201de8d3327c04ec167b0205009266833b79b4b
YourName <jonas.sundberg@slu.se>
2023-12-17 19:48:45 +0100

    Merge tracks

--- code/postprocess/merge_tracks.py
+++ code/postprocess/merge_tracks.py
@@ -8,6 +8,7 @@ import numpy as np
 import seaborn as sns
 import warnings
 warnings.filterwarnings('ignore')
+import os
 
 
 # Read tracks from bytetrack, botsort or similar
@@ -17,112 +18,133 @@ warnings.filterwarnings('ignore')
 
 
 # Arguments: 
-inputfile = "inference/Auklab1_FAR3_2022-07-08_05.00.00_560_580.mp4_bytetrack_custom.csv"
-track_merge_thres = 200
-track_assign_thres = 200 
-time_space_scale = .1
+inputfold = "inference/"
+#inputfile = "inference/Auklab1_FAR3_2022-07-08_05.00.00_560_580.mp4_bytetrack_custom.csv"
 
-dat = pd.read_csv(inputfile)
-dat["time2"] = pd.to_datetime(dat["time"]*1000*1000*1000)
+files = os.listdir(inputfold)
 
-# Stats for each track 
-trackdat = dat[dat["track_id"] != -1]
+for file in files: 
 
-trackstats = trackdat.groupby(["track_id"], as_index = False).aggregate({
-    "frame": ["first", "last"], 
-    "x": ["first", "last"],
-    "y": ["first", "last"],
-    })
+    filedata = file.split("_")
+    newname = filedata[1]+"_"+filedata[2]+"_"+filedata[3]
 
-metric = [0]
-rows = range(1, len(trackstats))
-for row in rows:
-    x1 = trackstats["x"]["first"][row]-trackstats["x"]["last"][row-1]
-    x2 = trackstats["x"]["last"][row]-trackstats["x"]["first"][row-1]
-    y1 = trackstats["y"]["first"][row]-trackstats["y"]["last"][row-1]
-    y2 = trackstats["y"]["last"][row]-trackstats["y"]["first"][row-1]
-    z1 = trackstats["frame"]["first"][row]-trackstats["frame"]["last"][row-1]
-    z2 = trackstats["frame"]["last"][row]-trackstats["frame"]["first"][row-1]
-    dist0 = np.sqrt(x1**2 + y1**2)
-    dist1 = np.sqrt(x2**2 +y2**2)
-    mindist = time_space_scale*(min(dist0, dist1))
-    minelapse = min(abs(z1), abs(z2))
-    metric.append(mindist*minelapse)
+    track_merge_thres = 200
+    track_assign_thres = 200 
+    time_space_scale = .1
 
+    dat = pd.read_csv(inputfold+file)
+    dat["time2"] = pd.to_datetime(dat["time"]*1000*1000*1000)
 
-trackstats["metric"] = metric
-trackstats["merge"] = np.where(trackstats["metric"] < track_merge_thres, True, False)
+    # Stats for each track 
+    trackdat = dat[dat["track_id"] != -1]
 
-# Merge 
+    trackstats = trackdat.groupby(["track_id"], as_index = False).aggregate({
+        "frame": ["first", "last"], 
+        "x": ["first", "last"],
+        "y": ["first", "last"],
+        })
 
-rows = range(1, len(trackstats))
-
-newtrack = [trackstats["track_id"][0]]
-for row in rows:
-    if trackstats["merge"][row] == True: 
-        newtrack.append(newtrack[row-1])
-    else: 
-        newtrack.append(trackstats["track_id"][row])
-    
-
-trackstats["newtrack"] = newtrack
-df = pd.DataFrame(trackstats[["track_id", "newtrack"]]).droplevel(1, axis = 1)
-
-
-# Combine with original df
-dat = dat.merge(df, on = "track_id", how = "left")
-
-
-# Go through df, check if unassigned points are close enough to tracks before or after
-
-rows = range(0, len(dat))
-newtrack2 = []
-rowprocess = []
-for row in rows:
-    if dat["track_id"][row] < 0:
-        temp = dat.iloc[row]
-        firstpoint = dat[row:][dat["track_id"] > 0].iloc[0]
-        lastpoint = dat[:row][dat["track_id"] > 0].iloc[0]
-        x1 = temp["x"]-firstpoint["x"]
-        x2 = temp["x"]-lastpoint["x"]
-        y1 = temp["y"]-firstpoint["y"]
-        y2 = temp["y"]-lastpoint["y"]
-        z1 = temp["frame"]-firstpoint["frame"]
-        z2 = temp["frame"]-lastpoint["frame"]
+    metric = [0]
+    rows = range(1, len(trackstats))
+    for row in rows:
+        x1 = trackstats["x"]["first"][row]-trackstats["x"]["last"][row-1]
+        x2 = trackstats["x"]["last"][row]-trackstats["x"]["first"][row-1]
+        y1 = trackstats["y"]["first"][row]-trackstats["y"]["last"][row-1]
+        y2 = trackstats["y"]["last"][row]-trackstats["y"]["first"][row-1]
+        z1 = trackstats["frame"]["first"][row]-trackstats["frame"]["last"][row-1]
+        z2 = trackstats["frame"]["last"][row]-trackstats["frame"]["first"][row-1]
         dist0 = np.sqrt(x1**2 + y1**2)
         dist1 = np.sqrt(x2**2 +y2**2)
-        elapse0 = abs(z1)
-        elapse1 = abs(z2)
-        scores = [np.mean([dist0, elapse0*10]), np.mean([dist1, elapse1*10])]
-        minval = min(scores)
-        mincategory = str(np.where(scores.index(minval) == 0, "first", "last"))
-        if minval < track_assign_thres: 
-            if mincategory == "first":
-                newtrack2.append(firstpoint["newtrack"])
-            else: 
-                newtrack2.append(lastpoint["newtrack"])
+        mindist = time_space_scale*(min(dist0, dist1))
+        minelapse = min(abs(z1), abs(z2))
+        metric.append(mindist*minelapse)
+
+
+    trackstats["metric"] = metric
+    trackstats["merge"] = np.where(trackstats["metric"] < track_merge_thres, True, False)
+
+    # Merge 
+
+    rows = range(1, len(trackstats))
+
+    newtrack = [trackstats["track_id"][0]]
+    for row in rows:
+        if trackstats["merge"][row] == True: 
+            newtrack.append(newtrack[row-1])
         else: 
-            newtrack2.append(temp["track_id"])
-    else: 
-        newtrack2.append(dat["newtrack"][row])
-    rowprocess.append(row)
+            newtrack.append(trackstats["track_id"][row])
+        
 
-dat["newtrack2"] = newtrack2
-
-dat.to_csv(inputfile[:-4]+"V2.csv")
+    trackstats["newtrack"] = newtrack
+    df = pd.DataFrame(trackstats[["track_id", "newtrack"]]).droplevel(1, axis = 1)
 
 
+    # Combine with original df
+    dat = dat.merge(df, on = "track_id", how = "left")
 
-# Plot 
 
-# Plot most recent track 
-palette = sns.color_palette("bright")
-sns.set(rc = {'axes.facecolor': 'white'})
-ax = sns.scatterplot(x= dat["x"], y=dat["y"], hue = dat["newtrack2"].astype("int"), palette = palette)
-ax.invert_yaxis()
-ax.grid(False)
-plt.show()
+    # Go through df, check if unassigned points are close enough to tracks before or after
 
-#plt.savefig("temp/"+datetime.datetime.now().strftime("%Y%m%dT%H%M%S"))
-#plt.close()
+    rows = range(0, len(dat))
+    newtrack2 = []
+    rowprocess = []
+    for row in rows:
+        if dat["track_id"][row] < 0:
+            temp = dat.iloc[row]
+            first, last = np.nan, np.nan 
+            if len(dat[row:][dat["track_id"] > 0]):
+                first = dat[row:][dat["track_id"] > 0].iloc[0]
+                x1, y1, z1 = temp["x"]-first["x"], temp["y"]-first["y"], temp["frame"]-first["frame"]
+            else: x1, y1, z1 = np.nan, np.nan, np.nan
+            if len(dat[:row][dat["track_id"] > 0]):
+                last = dat[:row][dat["track_id"] > 0].iloc[0]
+                x2, y2, z2 = temp["x"]-last["x"], temp["y"]-last["y"], temp["frame"]-last["frame"]
+            else: x2, y2, z2 = np.nan, np.nan, np.nan
+            
+            dist0 = np.sqrt(x1**2 + y1**2)
+            dist1 = np.sqrt(x2**2 +y2**2)
+            elapse0 = abs(z1)
+            elapse1 = abs(z2)
+            scores = [np.nanmean([dist0, elapse0*10]), np.nanmean([dist1, elapse1*10])]
+            minval = min(scores)
+            mincategory = str(np.where(scores.index(minval) == 0, "first", "last"))
+            if minval < track_assign_thres: 
+                if mincategory == "first":
+                    newtrack2.append(first["newtrack"])
+                else: 
+                    newtrack2.append(last["newtrack"])
+            else: 
+                newtrack2.append(temp["track_id"])
+        else: 
+            newtrack2.append(dat["newtrack"][row])
+        rowprocess.append(row)
+
+    dat["newtrack2"] = newtrack2
+
+    dat.to_csv("inference/_"+newname+".csv")
+
+
+    # Plot 
+
+    # Plot most recent track 
+    palette = sns.color_palette("bright")
+    sns.set(rc = {'axes.facecolor': 'white'})
+    ax = sns.scatterplot(x= dat["x"], y=dat["y"], hue = dat["newtrack2"].astype("int"), palette = palette)
+    ax.invert_yaxis()
+    ax.grid(False)
+    plt.savefig("temp/"+"tracks_space_"+newname+".jpg")
+    plt.close()
+
+
+
+    # Plot most recent track 
+    palette = sns.color_palette("bright")
+    sns.set(rc = {'axes.facecolor': 'white'})
+    ax = sns.lineplot(x= dat["time2"], y=dat["y"], hue = dat["newtrack2"].astype("int"), palette = palette)
+    ax.invert_yaxis()
+    ax.grid(False)
+    plt.savefig("temp/"+"tracks_time_"+newname+".jpg")
+    plt.close()
+
+
 
