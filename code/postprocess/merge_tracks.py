bf0ebc8ac3468b5b2742d730ae496fb5a955aab2
YourName <jonas.sundberg@slu.se>
2023-12-17 19:14:17 +0100

    merge tracks

--- code/postprocess/merge_tracks.py
+++ code/postprocess/merge_tracks.py
@@ -5,22 +5,35 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
 
-dat = pd.read_csv("inference/Auklab1_FAR3_2022-07-08_05.00.00_560_580.mp4_bytetrack_custom.csv")
+
+# Read tracks from bytetrack, botsort or similar
+# First merge similar tracks based on threshold
+# Then possibly assign untracked detections to existing tracks 
+# Saving new csv in the end  
+
+
+# Arguments: 
+inputfile = "inference/Auklab1_FAR3_2022-07-08_05.00.00_560_580.mp4_bytetrack_custom.csv"
+track_merge_thres = 200
+track_assign_thres = 200 
+time_space_scale = .1
+
+dat = pd.read_csv(inputfile)
 dat["time2"] = pd.to_datetime(dat["time"]*1000*1000*1000)
 
 # Stats for each track 
 trackdat = dat[dat["track_id"] != -1]
 
-trackstats = trackdat.groupby(["track_id"]).aggregate({
+trackstats = trackdat.groupby(["track_id"], as_index = False).aggregate({
     "frame": ["first", "last"], 
     "x": ["first", "last"],
     "y": ["first", "last"],
-    }).reset_index()
+    })
 
-dist = [0]
-elapse = [0]
-comb = [0]
 metric = [0]
 rows = range(1, len(trackstats))
 for row in rows:
@@ -32,14 +45,13 @@ for row in rows:
     z2 = trackstats["frame"]["last"][row]-trackstats["frame"]["first"][row-1]
     dist0 = np.sqrt(x1**2 + y1**2)
     dist1 = np.sqrt(x2**2 +y2**2)
-    mindist = .1*(min(dist0, dist1))
+    mindist = time_space_scale*(min(dist0, dist1))
     minelapse = min(abs(z1), abs(z2))
     metric.append(mindist*minelapse)
 
 
-
 trackstats["metric"] = metric
-trackstats["merge"] = np.where(trackstats["metric"] < 100, True, False)
+trackstats["merge"] = np.where(trackstats["metric"] < track_merge_thres, True, False)
 
 # Merge 
 
@@ -52,16 +64,65 @@ for row in rows:
     else: 
         newtrack.append(trackstats["track_id"][row])
     
-trackstats["newtrack"] = newtrack
 
+trackstats["newtrack"] = newtrack
+df = pd.DataFrame(trackstats[["track_id", "newtrack"]]).droplevel(1, axis = 1)
+
+
+# Combine with original df
+dat = dat.merge(df, on = "track_id", how = "left")
+
+
+# Go through df, check if unassigned points are close enough to tracks before or after
+
+rows = range(0, len(dat))
+newtrack2 = []
+rowprocess = []
+for row in rows:
+    if dat["track_id"][row] < 0:
+        temp = dat.iloc[row]
+        firstpoint = dat[row:][dat["track_id"] > 0].iloc[0]
+        lastpoint = dat[:row][dat["track_id"] > 0].iloc[0]
+        x1 = temp["x"]-firstpoint["x"]
+        x2 = temp["x"]-lastpoint["x"]
+        y1 = temp["y"]-firstpoint["y"]
+        y2 = temp["y"]-lastpoint["y"]
+        z1 = temp["frame"]-firstpoint["frame"]
+        z2 = temp["frame"]-lastpoint["frame"]
+        dist0 = np.sqrt(x1**2 + y1**2)
+        dist1 = np.sqrt(x2**2 +y2**2)
+        elapse0 = abs(z1)
+        elapse1 = abs(z2)
+        scores = [np.mean([dist0, elapse0*10]), np.mean([dist1, elapse1*10])]
+        minval = min(scores)
+        mincategory = str(np.where(scores.index(minval) == 0, "first", "last"))
+        if minval < track_assign_thres: 
+            if mincategory == "first":
+                newtrack2.append(firstpoint["newtrack"])
+            else: 
+                newtrack2.append(lastpoint["newtrack"])
+        else: 
+            newtrack2.append(temp["track_id"])
+    else: 
+        newtrack2.append(dat["newtrack"][row])
+    rowprocess.append(row)
+
+dat["newtrack2"] = newtrack2
+
+dat.to_csv(inputfile[:-4]+"V2.csv")
 
 
 
 # Plot 
-fig, ax = plt.subplots()
 
-ax.scatter(trackdat["time2"], trackdat["x"], color = "black")
-ax.scatter(trackdat["time2"], trackdat["y"], color = "red")
+# Plot most recent track 
+palette = sns.color_palette("bright")
+sns.set(rc = {'axes.facecolor': 'white'})
+ax = sns.scatterplot(x= dat["x"], y=dat["y"], hue = dat["newtrack2"].astype("int"), palette = palette)
+ax.invert_yaxis()
+ax.grid(False)
 plt.show()
 
+#plt.savefig("temp/"+datetime.datetime.now().strftime("%Y%m%dT%H%M%S"))
+#plt.close()
 
