3530b26fc3f84b419287b8638de67c3c13dcf366
hentatisundberg <jonas.sundberg@slu.se>
2024-02-09 20:09:48 +0100

    improved runcode

--- code/yolov8/yolo_savevid.py
+++ code/yolov8/yolo_savevid.py
@@ -2,13 +2,82 @@ import pandas as pd
 from pathlib import Path 
 from ultralytics import YOLO
 import shutil
-
-# Video compression using os
-
 import cv2
-from pathlib import Path
+import os
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+import sqlite3
+import numpy as np
 
-def compress_vids(input, outputdir):
+# Functions
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+
+    return conn
+
+
+def cut_vid(row, vidpath, savepath): 
+
+    file = row
+
+    #print(ind)
+    datefold = str(file["start"])[0:10]
+    ledge = file["Ledge"]
+    yr = str(file["start"])[0:4]
+
+    starttime = str(file["file"][:-4]).split("_")[3].replace(".", ":")
+    startclip = file["start"]
+    endclip = file["end"]
+
+    if any(pd.isnull([startclip, endclip, starttime])):
+        print("skip")
+
+    else: 
+        starttimestamp = pd.to_datetime(datefold+" "+starttime)
+
+        startsec = (file["start"]-starttimestamp)/np.timedelta64(1,'s')
+        endsec = (file["end"]-starttimestamp)/np.timedelta64(1,'s')
+
+        vid_rel_path = f"{vidpath}Video{yr}/{ledge}/{datefold}/"
+        #vid_rel_path = f"{vidpath}/{datefold}/"
+        full_path = vid_rel_path+file["file"]
+
+        if os.path.isfile(full_path):
+
+            filename_out = f"{savepath}{file['track']}.mp4"
+            ffmpeg_extract_subclip(
+                full_path,
+                startsec,
+                endsec,
+                targetname = filename_out
+            )
+            print(filename_out)
+            return(filename_out)
+    
+
+def annotate_vid(vid, model):
+
+    # Pick out relevant video information
+    results = model(vid, 
+                    conf = 0.1,
+                    stream=True,  
+                    save = True,
+                    show = False, 
+                    save_frames = False)
+    for result in results: 
+        result.boxes
+
+def compress_vid(input, outputdir):
 
     file = Path(input)
     name = file.name
@@ -20,13 +89,11 @@ def compress_vids(input, outputdir):
     if not cap.isOpened():
         print("Error: Could not open the input video file.")
         exit()
-
-    fourcc = cv2.VideoWriter_fourcc(*'H264')  # Change this to your desired codec
+    # XVID better than MJPG. DIVX = XVID
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')  # Change this to your desired codec
     frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
-    print(frame_size)
-    print(frame_rate)
-
+    
     out = cv2.VideoWriter(output, fourcc, frame_rate, frame_size, isColor=True)
 
     while(cap.isOpened()):
@@ -45,40 +112,47 @@ def compress_vids(input, outputdir):
     out.release()
     cv2.destroyAllWindows()
 
-
-# Detection models and trackers 
-tracker = "ultralytics/cfg/trackers/bytetrack.yaml"
-tracker_name = tracker.split("/")[-1].split(".")[0]
-#model = YOLO("../../../../../../mnt/BSP_NAS2_work/fish_model/models/best_train57.pt")
-
-# Work folders
-input_dir = Path("../../../../../../mnt/BSP_NAS2_work/fish_model/clips3")
-#temp_save_dir = Path("../../../../../../mnt/BSP_NAS2_work/fish_model/clips_temp") 
-save_dir = Path("../../../../../../mnt/BSP_NAS2_work/fish_model/clips_annot2")
+def cleanup(folder):
+    shutil.rmtree(f"runs/detect/{folder}/{Path(vid).stem}_frames") # Remove frames
+    os.remove(f"runs/detect/{folder}/{Path(vid).stem}.avi") # Remove video from Larus 
+    shutil.rmtree(f"runs/detect/{folder}") # Remove folder
+    os.remove(f"../../../../../mnt/BSP_NAS2_work/fish_model/t0/{Path(vid).name}") # Remove video from Larus 
 
 
-vids = list(input_dir.glob("*.mp4"))
+def df_from_db(db, scale, minframes, maxframes):
+    # Create connection
+    con = create_connection(db)
+
+    cond1 = f'Ledge == "{scale}"'
+    cond2 = f'nframes > {minframes}'
+    cond3 = f'nframes < {maxframes}'
+
+    sql = (f'SELECT * '
+           f'FROM Inference '
+           f'WHERE {cond1} AND {cond2} AND {cond3};')
+
+    df = pd.read_sql_query(
+        sql,
+        con, 
+        parse_dates = {"start": "%Y-%m-%d %H:%M:%S.%f", "end": "%Y-%m-%d %H:%M:%S.%f"}
+        )
+    
+    return(df)
 
 
-# CUT IN THE SAME SCRIPT!
+def main(ledge, minframes, maxframes):
 
-def annotate_vids(vids):
+    df = df_from_db("inference/Inference_stats_nomerge.db", ledge, minframes, maxframes)
+    folder = "predict2"
 
-    for vid in vids: 
-
-        # Pick out relevant video information
-        results = model(vid, 
-                        conf = 0.1,
-                        stream=True,  
-                        save = True,
-                        show = False, 
-                        save_frames = False)
-        for result in results: 
-            result.boxes
-        shutil.rmtree(f"runs/detect/predict13/{vid.stem}_frames") # Remove frames
-        compress_vids(f"runs/detect/predict13/{vid.stem}.avi", Path("../../../../../../mnt/BSP_NAS2_work/fish_model/clips_annot2/"))
+    for row in df.index:
+        input = df.iloc[row]
+        vid = cut_vid(input, "../../../../../mnt/BSP_NAS2/Video/", "../../../../../mnt/BSP_NAS2_work/fish_model/t0/")
+        annotate_vid(vid, YOLO("../../../../../../mnt/BSP_NAS2_work/fish_model/models/best_train57.pt"))
+        compress_vid(f"runs/detect/{folder}/{Path(vid).stem}.avi", "../../../../../../mnt/BSP_NAS2_work/fish_model/clips_annot4/")
+        cleanup(folder)
+        print("Unknown error")
 
 
-
-#compress_vids(f"runs/detect/predict13/BONDEN322061505-3.avi", Path("runs/detect/predict/"))
-compress_vids(f"vids/orig/BONDEN322062105-4.avi", "vids/compressed/")
+# RUN 
+main("FAR3", 0, 20)
