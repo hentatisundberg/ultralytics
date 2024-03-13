8030b71e63dbcbea1b504fdc045e9a7985f9a782
hentatisundberg <jonas.sundberg@slu.se>
2024-03-13 08:15:21 +0100

    update paths

--- code/misc/check_path.py
+++ code/misc/check_path.py
@@ -1,7 +1,8 @@
 
 import os
 
-print(os.listdir("../../../../../../Volumes/JHS-SSD2/"))
+#print(os.listdir("../../../../../../Volumes/JHS-SSD2/"))
+print(os.listdir("../../../../../../mnt/BSP_NAS2_work/fish_model/inference"))
 
 
 
--- code/postprocess/preprocess_raw_tracks.py
+++ code/postprocess/preprocess_raw_tracks.py
@@ -2,21 +2,15 @@
 
 
 import pandas as pd
-import numpy as np
-import os
 from pathlib import Path
-import matplotlib.pyplot as plt
-import sys
-import sqlite3
-from datetime import datetime 
 from functions import euclidean, create_connection, associate_points_before, calc_stats2, modify_output, prep_data, insert_to_db, df_from_db, modify_input
 from plot_functions import plot_raw_by_time
 
 
 # This scripts runs assign_points_before on raw output (with tracks for some but not all detections) and saves a db
 
-path = Path("inference/orig/")
-files = list(path.glob("*FAR3_2023-07-01_17*"))
+path = Path("../../../../../../mnt/BSP_NAS2_work/fish_model/inference")
+files = list(path.glob("*.csv"))
 for file in files: 
     df = pd.read_csv(file)
     df_track = df[df["track_id"] != -1]
@@ -25,25 +19,5 @@ for file in files:
         assigned = associate_points_before(df, 1, 200, 20)
         assigned2 = modify_input(assigned)
         stats = calc_stats2(assigned2, "track")
-        insert_to_db(assigned2, "inference/Inference_raw_nomergeV3.db")
-        insert_to_db(stats, "inference/Inference_stats_nomergeV3.db")
-
-#t1 = assigned[assigned["track_id"] == 78952.0]
-#t1["frame"]
-#test = t1["frame"]
-
-#newvals = test.diff().tolist()[1:]
-#newvals.insert(0, 1.0)
-#pnewvals = pd.Series(newvals)
-#newtest = pd.Series(newvals) > 0
-
-#df[np.array([0,1,0,0,1,1,0,0,0,1],dtype=bool)]
-
-#t1.loc[:, pnewvals > 0]
-
-#t1.loc[newtest]
-
-
-#fig, ax = plt.subplots()
-#ax.scatter(list(range(0, 26)), t1["frame"])
-#plt.show()
+        insert_to_db(assigned2, "inference/Inference_raw_nomergeZ.db")
+        insert_to_db(stats, "inference/Inference_stats_nomergeZ.db")
