e3df134492ca5dd9ac8d86ac4c85219279f052ed
YourName <jonas.sundberg@slu.se>
2025-02-26 11:18:04 +0100

    code for moving files

---
+++ code/dataset/move_files.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+from pathlib import Path
+
+base_dir = Path("../../../../../../mnt/BSP_NAS2_vol3/Video/Video2024/")
+inputfiles = pd.read_csv("data/filenames.csv")
+output_dir = Path("../../../../../../mnt/BSP_NAS2_work/temp/vid")
+
+for row in inputfiles.index:
+    vid = Path(inputfiles.loc[row, "path"])
+    vid_origin = base_dir.joinpath(vid)
+    vid_dest = output_dir.joinpath(vid.name)
+    print (vid_dest)
+    print (vid_origin)
+    # Copy file from origin to destination
+    vid_origin.rename(vid_dest)
+    
+    
